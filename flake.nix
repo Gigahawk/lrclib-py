59e5190920f8e80ec8d96aa98da20d8715180479
Jasper Chan <jasperchan515@gmail.com>
2026-06-03 02:42:13 -0700

    feature: build and devshell working

--- flake.nix
+++ flake.nix
@@ -5,16 +5,39 @@
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-    uv2nix.url = "github:Gigahawk/uv2nix/requires-python-test";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts-python = {
-      url = "github:Gigahawk/flake-parts-python";
+    pyproject-nix = {
+      url = "github:Gigahawk/pyproject.nix/py3.8";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.uv2nix.follows = "uv2nix";
+    };
+    uv2nix = {
+      url = "github:Gigahawk/uv2nix/disable-requires-python";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    pyproject-build-systems = {
+      url = "github:Gigahawk/build-system-pkgs/py3.8";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    flake-parts-python = {
+      url = "github:Gigahawk/flake-parts-python/py3.8";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        pyproject-build-systems.follows = "pyproject-build-systems";
+      };
     };
   };
 
